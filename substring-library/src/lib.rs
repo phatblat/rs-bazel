05d680e17940fac5ef54d8b11b05f872214ceb64
Ben Chatelain <ben@octop.ad>
2024-11-07 07:21:04 -0700

    🔥 Remove dup tests mod block

--- substring-library/src/lib.rs
+++ substring-library/src/lib.rs
@@ -2,17 +2,6 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
-
 pub fn find_substring<'a>(s: &'a str, substring: &str) -> Option<&'a str> {
     s.find(substring).map(|i| &s[i..i + substring.len()])
 }
